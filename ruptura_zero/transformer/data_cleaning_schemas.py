d80c4e845542dc8ffd480924f8f18af0389314f7
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2025-08-28 09:51:53 -0300

    fix(transformer): update data types and pandera schemas

--- ruptura_zero/transformer/data_cleaning_schemas.py
+++ ruptura_zero/transformer/data_cleaning_schemas.py
@@ -19,15 +19,25 @@ DATA_CLEANING_SCHEMAS = [
     {
         'name': '01_BD_Ruptura',
         'data_attr': 'ruptura_data',
+        'columns': {
+            'DT_MES': 'data_base',
+            'COD_CLIENTE': 'cliente_id',
+            'CLIENTE_DESCRICAO': 'descricao_cliente',
+            'MATERIAL_DESCRICAO_CATEGORIA': 'categoria_material',
+            'Valor Ruptura_$': 'valor_ruptura',
+            'Valor Pedido_$': 'valor_pedido',
+            'Volume ruptura_und': 'volume_ruptura_und',
+            'Ruptura_%': 'percent_ruptura'
+        },
         'types': {
-            'dt_mes': 'date',
-            'cod_cliente': 'string',
-            'cliente_descricao': 'string',
-            'material_descricao_categoria': 'string',
-            'valor_ruptura_$': 'monetary',
-            'valor_pedido_$': 'monetary',
+            'data_base': 'data-base',
+            'cliente_id': 'string',
+            'descricao_cliente': 'string',
+            'categoria_material': 'string',
+            'valor_ruptura': 'monetary',
+            'valor_pedido': 'monetary',
             'volume_ruptura_und': 'integer',
-            'ruptura_%': 'percent',
+            'percent_ruptura': 'percent',
             'ano': 'integer',
             'mes': 'integer'
         },
@@ -36,32 +46,51 @@ DATA_CLEANING_SCHEMAS = [
     {
         'name': '02_BD_Estoque',
         'data_attr': 'estoque_data',
+        'columns': {
+            'MES': 'mes',
+            'COD_CLIENTE': 'cliente_id',
+            'NOME CLIENTE': 'nome_cliente',
+            'DESCRICAO_CATEGORIA': 'categoria_material',
+            'ESTOQUE': 'estoque',
+            'DDV': 'ddv',
+            'COBERTURA_DIAS': 'cobertura_dias',
+            'TIPO_CLIENTE': 'tipo_cliente',
+            'CONTATO CLIENTE': 'contato_cliente'
+        },
         'types': {
             'mes': 'string',
-            'cod_cliente': 'string',
+            'cliente_id': 'string',
             'nome_cliente': 'string',
-            'descricao_categoria': 'string',
+            'categoria_material': 'string',
             'estoque': 'integer',
             'ddv': 'float',
             'cobertura_dias': 'integer',
             'tipo_cliente': 'string',
             'contato_cliente': 'string'
         },
-        'pandera_schema': VENDAS_SCHEMA
+        'pandera_schema': ESTOQUE_SCHEMA
     },
     {
         'name': '03_BD_Vendas',
         'data_attr': 'vendas_data',
+        'columns': {
+            'DT_MES': 'data_base',
+            'COD CLIEN': 'cliente_id',
+            'VLR_VOLUME_REAL': 'valor_volume_real',
+            'CIDADE': 'cidade',
+            'UF': 'uf',
+            'PAIS': 'pais'
+        },
         'types': {
-            'dt_mes': 'date',
-            'cod_clien': 'string',
-            'vlr_volume_real': 'integer',
+            'data_base': 'data-base',
+            'cliente_id': 'string',
+            'valor_volume_real': 'string',
             'cidade': 'string',
             'uf': 'string',
-            'pais': 'float',
+            'pais': 'string',
             'ano': 'integer',
             'mes': 'integer'
         },
-        'pandera_schema': ESTOQUE_SCHEMA
-    }
+        'pandera_schema': VENDAS_SCHEMA
+    },
 ]
--- ruptura_zero/transformer/pandera_schemas.py
+++ ruptura_zero/transformer/pandera_schemas.py
@@ -18,14 +18,14 @@ import pandera.pandas as pa
 # Esquema de validação para os dados de ruptura.
 RUPTURA_SCHEMA = pa.DataFrameSchema(
     columns={
-        'dt_mes': pa.Column(int, nullable=False, coerce=True),
-        'cod_cliente': pa.Column(str, nullable=False, coerce=True),
-        'cliente_descricao': pa.Column(str, nullable=False, coerce=True),
-        'material_descricao_categoria': pa.Column(str, nullable=False, coerce=True),
-        'valor_ruptura_$': pa.Column(float, nullable=False, coerce=True),
-        'valor_pedido_$': pa.Column(float, nullable=False, coerce=True),
+        'data_base': pa.Column(int, nullable=False, coerce=True),
+        'cliente_id': pa.Column(str, nullable=False, coerce=True),
+        'descricao_cliente': pa.Column(str, nullable=False, coerce=True),
+        'categoria_material': pa.Column(str, nullable=False, coerce=True),
+        'valor_ruptura': pa.Column(float, nullable=False, coerce=True),
+        'valor_pedido': pa.Column(float, nullable=False, coerce=True),
         'volume_ruptura_und': pa.Column(int, nullable=False, coerce=True),
-        'ruptura_%': pa.Column(float, nullable=False, coerce=True),
+        'percent_ruptura': pa.Column(float, nullable=False, coerce=True),
         'ano': pa.Column(int, nullable=False, coerce=True),
         'mes': pa.Column(int, nullable=False, coerce=True),
     },
@@ -36,14 +36,15 @@ RUPTURA_SCHEMA = pa.DataFrameSchema(
 # Esquema de validação para os dados de estoque.
 ESTOQUE_SCHEMA = pa.DataFrameSchema(
     columns={
-        'dt_mes': pa.Column(int, nullable=False, coerce=True),
-        'cod_clien': pa.Column(str, nullable=False, coerce=True),
-        'vlr_volume_real': pa.Column(int, nullable=False, coerce=True),
-        'cidade': pa.Column(str, nullable=False, coerce=True),
-        'uf': pa.Column(str, nullable=False, coerce=True),
-        'pais': pa.Column(str, nullable=False, coerce=True),
-        'ano': pa.Column(int, nullable=False, coerce=True),
-        'mes': pa.Column(int, nullable=False, coerce=True),
+        'mes': pa.Column(str, nullable=False, coerce=True),
+        'cliente_id': pa.Column(str, nullable=False, coerce=True),
+        'nome_cliente': pa.Column(str, nullable=False, coerce=True),
+        'categoria_material': pa.Column(str, nullable=False, coerce=True),
+        'estoque': pa.Column(int, nullable=False, coerce=True),
+        'ddv': pa.Column(float, nullable=False, coerce=True),
+        'cobertura_dias': pa.Column(int, nullable=False, coerce=True),
+        'tipo_cliente': pa.Column(str, nullable=False, coerce=True),
+        'contato_cliente': pa.Column(str, nullable=False, coerce=True),
     },
     strict=True,
     ordered=True
@@ -52,15 +53,14 @@ ESTOQUE_SCHEMA = pa.DataFrameSchema(
 # Esquema de validação para os dados de vendas.
 VENDAS_SCHEMA = pa.DataFrameSchema(
     columns={
-        'mes': pa.Column(str, nullable=False, coerce=True),
-        'cod_cliente': pa.Column(str, nullable=False, coerce=True),
-        'nome_cliente': pa.Column(str, nullable=False, coerce=True),
-        'descricao_categoria': pa.Column(str, nullable=False, coerce=True),
-        'estoque': pa.Column(int, nullable=False, coerce=True),
-        'ddv': pa.Column(float, nullable=False, coerce=True),
-        'cobertura_dias': pa.Column(int, nullable=False, coerce=True),
-        'tipo_cliente': pa.Column(str, nullable=False, coerce=True),
-        'contato_cliente': pa.Column(str, nullable=False, coerce=True),
+        'data_base': pa.Column(int, nullable=False, coerce=True),
+        'cliente_id': pa.Column(str, nullable=False, coerce=True),
+        'valor_volume_real': pa.Column(float, nullable=False, coerce=True),
+        'cidade': pa.Column(str, nullable=False, coerce=True),
+        'uf': pa.Column(str, nullable=False, coerce=True),
+        'pais': pa.Column(str, nullable=False, coerce=True),
+        'ano': pa.Column(int, nullable=False, coerce=True),
+        'mes': pa.Column(int, nullable=False, coerce=True),
     },
     strict=True,
     ordered=True
