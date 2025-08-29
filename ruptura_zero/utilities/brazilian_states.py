44f03f6f795ba04d75754eb54a257f71ba51c9c6
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2025-08-29 09:06:25 -0300

    feat: add StrEnum for validate Brazilian states in pandera schema

---
+++ ruptura_zero/utilities/brazilian_states.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# ------------------------------------------------------------------------------
+#  Name: brazilian_states.py
+#  Version: 0.0.1
+#
+#  Summary: Ruptura Zero: Análise de Vendas e Estoque
+#
+#  Author: Alexsander Lopes Camargos
+#  Author-email: alcamargos@vivaldi.net
+#
+#  License: MIT
+# ------------------------------------------------------------------------------
+
+from enum import StrEnum
+
+
+class BrazilianStates(StrEnum):
+    AC = 'AC'
+    AL = 'AL'
+    AP = 'AP'
+    AM = 'AM'
+    BA = 'BA'
+    CE = 'CE'
+    DF = 'DF'
+    ES = 'ES'
+    GO = 'GO'
+    MA = 'MA'
+    MT = 'MT'
+    MS = 'MS'
+    MG = 'MG'
+    PA = 'PA'
+    PB = 'PB'
+    PR = 'PR'
+    PE = 'PE'
+    PI = 'PI'
+    RJ = 'RJ'
+    RN = 'RN'
+    RS = 'RS'
+    RO = 'RO'
+    RR = 'RR'
+    SC = 'SC'
+    SP = 'SP'
+    SE = 'SE'
+    TO = 'TO'
